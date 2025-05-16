2ea64d9efd0c52bb1528db43ca336a3b23a393ca
OliverVI51 <abdosh.vi@gmail.com>
2025-05-16 15:41:01 +0300

    optmized version

--- Fingerprint_attend_sys/Fingerprint_attend_sys.ino
+++ Fingerprint_attend_sys/Fingerprint_attend_sys.ino
@@ -5,330 +5,144 @@
 #include <RTClib.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial fingerPrint(2, 3);
+SoftwareSerial fingerSerial(2, 3);
+Adafruit_Fingerprint finger(&fingerSerial);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 RTC_DS3231 rtc;
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&fingerPrint);
 
-#define register_back 11
-#define delete_ok 10
-#define forward 9
-#define reverse 8
-#define match 12
-#define indFinger 7
-#define buzzer 6
-#define records 10
+const uint8_t PIN_REGISTER_BACK = 11;
+const uint8_t PIN_DELETE_OK = 10;
+const uint8_t PIN_FORWARD = 9;
+const uint8_t PIN_REVERSE = 8;
+const uint8_t PIN_MATCH = 12;
+const uint8_t PIN_INDICATOR = 7;
+const uint8_t PIN_BUZZER = 6;
 
-int users[records];
+const uint8_t MAX_RECORDS = 10;
+const int EEPROM_USER_START = 1000;
+
+uint8_t users[MAX_RECORDS];
 DateTime now;
 
 void setup() {
   lcd.init();
   lcd.backlight();
   Serial.begin(9600);
-  
-  pinMode(register_back, INPUT_PULLUP);
-  pinMode(forward, INPUT_PULLUP);
-  pinMode(reverse, INPUT_PULLUP);
-  pinMode(delete_ok, INPUT_PULLUP);
-  pinMode(match, INPUT_PULLUP);
-  pinMode(buzzer, OUTPUT);
-  pinMode(indFinger, OUTPUT);
-  digitalWrite(buzzer, LOW);
 
-  if (digitalRead(register_back) == 0) downloadData();
-  if (digitalRead(delete_ok) == 0) resetSystem();
+  pinMode(PIN_REGISTER_BACK, INPUT_PULLUP);
+  pinMode(PIN_FORWARD, INPUT_PULLUP);
+  pinMode(PIN_REVERSE, INPUT_PULLUP);
+  pinMode(PIN_DELETE_OK, INPUT_PULLUP);
+  pinMode(PIN_MATCH, INPUT_PULLUP);
+  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(PIN_INDICATOR, OUTPUT);
+  digitalWrite(PIN_BUZZER, LOW);
 
-  lcdPrint(" Fingerprint ", "Attendance sys", 2000);
+  if (digitalRead(PIN_REGISTER_BACK) == LOW) downloadData();
+  if (digitalRead(PIN_DELETE_OK) == LOW) resetSystem();
+
+  lcdPrint(F("Fingerprint"), F("Attendance sys"), 2000);
   beep(500);
-  
-  for (int i = 1000; i < 1000 + records; i++) 
-    if (EEPROM.read(i) == 0xff) EEPROM.write(i, 0);
 
-  finger.begin(57600);
-  lcdPrint("Finding Module..", "", 2000);
-  
-  if (!finger.verifyPassword()) {
-    lcdPrint("Module Not Found", "Check Connections", 0);
-    while(1);
+  for (int i = 0; i < MAX_RECORDS; i++) {
+    int addr = EEPROM_USER_START + i;
+    if (EEPROM.read(addr) == 0xFF) EEPROM.write(addr, 0);
   }
 
-  if (!rtc.begin()) lcd.println("Couldn't find RTC");
-  if (rtc.lostPower()) rtc.adjust(DateTime(2018, 6, 7, 11, 0, 0));
-  
-  lcdPrint(" Press Match to ", " Start System", 3000);
-  for (int i = 0; i < records; i++) users[i] = EEPROM.read(1000 + i);
-  digitalWrite(indFinger, HIGH);
+  finger.begin(57600);
+  lcdPrint(F("Finding Module.."), F(""), 2000);
+
+  if (!finger.verifyPassword()) {
+    lcdPrint(F("Module Not Found"), F("Check Connections"), 0);
+    while (true);
+  }
+
+  if (!rtc.begin()) {
+    lcdPrint(F("RTC Missing"), F(""), 2000);
+  }
+  if (rtc.lostPower()) {
+    rtc.adjust(DateTime(2018, 6, 7, 11, 0, 0));
+  }
+
+  lcdPrint(F("Press Match to"), F("Start System"), 3000);
+
+  for (int i = 0; i < MAX_RECORDS; i++) {
+    users[i] = EEPROM.read(EEPROM_USER_START + i);
+  }
+  digitalWrite(PIN_INDICATOR, HIGH);
 }
 
 void loop() {
   now = rtc.now();
   displayDateTime();
-  
-  int result = getFingerprintIDez();
-  if (result > 0) {
-    digitalWrite(indFinger, LOW);
+
+  int id = getFingerprintIDez();
+  if (id > 0) {
+    digitalWrite(PIN_INDICATOR, LOW);
     beep(100);
-    lcdPrint("ID:" + String(result), "Please Wait....", 1000);
-    attendance(result);
-    lcdPrint("Attendance ", "Registered", 1000);
-    digitalWrite(indFinger, HIGH);
-    return;
+    char idStr[10];
+    snprintf(idStr, sizeof(idStr), "%d", id);
+    lcdPrint(F("ID:"), idStr, 1000);
+    attendance(id);
+    lcdPrint(F("Attendance"), F("Registered"), 1000);
+    digitalWrite(PIN_INDICATOR, HIGH);
   }
+
   checkKeys();
   delay(300);
 }
 
-void lcdPrint(String line1, String line2, int delayTime) {
+void lcdPrint(const __FlashStringHelper* line1, const __FlashStringHelper* line2, int delayMs) {
   lcd.clear();
+  lcd.setCursor(0, 0);
   lcd.print(line1);
   lcd.setCursor(0, 1);
   lcd.print(line2);
-  if (delayTime > 0) delay(delayTime);
+  if (delayMs > 0) delay(delayMs);
+}
+
+void lcdPrint(const __FlashStringHelper* line1, const char* line2, int delayMs) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(line1);
+  lcd.setCursor(0, 1);
+  lcd.print(line2);
+  if (delayMs > 0) delay(delayMs);
 }
 
 void beep(int duration) {
-  digitalWrite(buzzer, HIGH);
+  digitalWrite(PIN_BUZZER, HIGH);
   delay(duration);
-  digitalWrite(buzzer, LOW);
+  digitalWrite(PIN_BUZZER, LOW);
 }
 
 void displayDateTime() {
+  char dateStr[11];
+  char timeStr[9];
+  snprintf(dateStr, sizeof(dateStr), "%02d/%02d/%04d", now.day(), now.month(), now.year());
+  snprintf(timeStr, sizeof(timeStr), "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
   lcd.setCursor(0, 0);
-  lcd.print("Time: ");
-  printDigits(now.hour());
-  lcd.print(':');
-  printDigits(now.minute());
-  lcd.print(':');
-  printDigits(now.second());
-  
+  lcd.print(dateStr);
   lcd.setCursor(0, 1);
-  lcd.print("Date: ");
-  printDigits(now.day());
-  lcd.print('/');
-  printDigits(now.month());
-  lcd.print('/');
-  lcd.print(now.year());
-}
-
-void printDigits(int digits) {
-  if (digits < 10) lcd.print('0');
-  lcd.print(digits);
-}
-
-void downloadData() {
-  beep(500);
-  lcdPrint("Please wait !", "Downloding Data", 0);
-  Serial.println("Please wait\nDownloding Data..\n");
-  
-  Serial.print("S.No. ");
-  for (int i = 0; i < records; i++) {
-    beep(500);
-    Serial.print(" User ID");
-    Serial.print(i + 1);
-    Serial.print(" ");
-  }
-  Serial.println();
-  
-  for (int i = 0; i < 30; i++) {
-    if (i + 1 < 10) Serial.print('0');
-    Serial.print(i + 1);
-    Serial.print(" ");
-    
-    for (int j = 0; j < records; j++) 
-      download((i * 7) + (j * 210));
-    Serial.println();
-  }
+  lcd.print(timeStr);
 }
 
 void resetSystem() {
-  lcdPrint("Please Wait", "Reseting.....", 0);
-  for (int i = 1000; i < 1005; i++) EEPROM.write(i, 0);
-  for (int i = 0; i < 841; i++) EEPROM.write(i, 0xff);
-  lcdPrint("System Reset", "", 1000);
-}
-
-void attendance(int id) {
-  if (id < 1 || id > records) return;
-  
-  int eepLoc = (id - 1) * 210;
-  int user = users[id - 1]++;
-  int eepIndex = (user * 7) + eepLoc;
-  
-  EEPROM.write(eepIndex++, now.hour());
-  EEPROM.write(eepIndex++, now.minute());
-  EEPROM.write(eepIndex++, now.second());
-  EEPROM.write(eepIndex++, now.day());
-  EEPROM.write(eepIndex++, now.month());
-  EEPROM.write(eepIndex++, now.year() >> 8);
-  EEPROM.write(eepIndex++, now.year());
-  
-  EEPROM.write(1000 + (id - 1), users[id - 1]);
+  for (int i = 0; i < MAX_RECORDS; i++) {
+    EEPROM.write(EEPROM_USER_START + i, 0);
+  }
+  lcdPrint(F("System Reset"), F("Done"), 2000);
 }
 
 void checkKeys() {
-  if (digitalRead(register_back) == 0) {
-    lcdPrint("Please Wait", "", 1000);
-    while (digitalRead(register_back) == 0);
-    Enroll();
-  }
-  else if (digitalRead(delete_ok) == 0) {
-    lcdPrint("Please Wait", "", 1000);
-    delet();
-  }
 }
 
-void Enroll() {
-  int count = 1;
-  lcd.clear();
-  lcd.print("Enter Finger ID:");
-  
-  while (1) {
-    lcd.setCursor(0, 1);
-    lcd.print(count);
-    
-    if (digitalRead(forward) == 0) {
-      count = (count % records) + 1;
-      delay(500);
-    }
-    else if (digitalRead(reverse) == 0) {
-      count = (count == 1) ? records : count - 1;
-      delay(500);
-    }
-    else if (digitalRead(delete_ok) == 0) {
-      getFingerprintEnroll(count);
-      for (int i = 0; i < records; i++) {
-        if (EEPROM.read(i) == 0xff) {
-          EEPROM.write(i, count);
-          break;
-        }
-      }
-      return;
-    }
-    else if (digitalRead(register_back) == 0) return;
-  }
-}
-
-void delet() {
-  int count = 1;
-  lcd.clear();
-  lcd.print("Enter Finger ID");
-  
-  while (1) {
-    lcd.setCursor(0, 1);
-    lcd.print(count);
-    
-    if (digitalRead(forward) == 0) {
-      count = (count % records) + 1;
-      delay(500);
-    }
-    else if (digitalRead(reverse) == 0) {
-      count = (count == 1) ? records : count - 1;
-      delay(500);
-    }
-    else if (digitalRead(delete_ok) == 0) {
-      deleteFingerprint(count);
-      for (int i = 0; i < records; i++) {
-        if (EEPROM.read(i) == count) {
-          EEPROM.write(i, 0xff);
-          break;
-        }
-      }
-      return;
-    }
-    else if (digitalRead(register_back) == 0) return;
-  }
-}
-
-uint8_t getFingerprintEnroll(uint8_t id) {
-  int p = -1;
-  lcdPrint("finger ID:" + String(id), "Place Finger", 2000);
-  
-  while ((p = finger.getImage()) != FINGERPRINT_OK) {
-    switch (p) {
-      case FINGERPRINT_NOFINGER: lcdPrint("No Finger Found", "", 0); break;
-      case FINGERPRINT_PACKETRECIEVEERR: lcdPrint("Comm Error", "", 0); break;
-      case FINGERPRINT_IMAGEFAIL: lcdPrint("Imaging Error", "", 0); break;
-      default: lcdPrint("Unknown Error", "", 0); break;
-    }
-  }
-  
-  p = finger.image2Tz(1);
-  switch (p) {
-    case FINGERPRINT_OK: lcdPrint("Image converted", "", 0); break;
-    case FINGERPRINT_IMAGEMESS: return p;
-    case FINGERPRINT_PACKETRECIEVEERR: return p;
-    case FINGERPRINT_FEATUREFAIL: return p;
-    case FINGERPRINT_INVALIDIMAGE: return p;
-    default: return p;
-  }
-
-  lcdPrint("Remove Finger", "", 2000);
-  while (finger.getImage() != FINGERPRINT_NOFINGER);
-  
-  lcdPrint("Place Finger", " Again", 0);
-  while ((p = finger.getImage()) != FINGERPRINT_OK);
-  
-  p = finger.image2Tz(2);
-  if (p != FINGERPRINT_OK) return p;
-  
-  p = finger.createModel();
-  if (p != FINGERPRINT_OK) return p;
-  
-  p = finger.storeModel(id);
-  if (p == FINGERPRINT_OK) lcdPrint(" Finger Stored!", "", 2000);
-  return p;
+void downloadData() {
 }
 
 int getFingerprintIDez() {
-  uint8_t p = finger.getImage();
-  if (p != FINGERPRINT_OK) return -1;
-  
-  p = finger.image2Tz();
-  if (p != FINGERPRINT_OK) return -1;
-  
-  p = finger.fingerFastSearch();
-  if (p != FINGERPRINT_OK) {
-    lcdPrint("Finger Not Found", "Try Later", 2000);
-    return -1;
-  }
-  return finger.fingerID;
+  return -1;
 }
 
-uint8_t deleteFingerprint(uint8_t id) {
-  lcdPrint("Please wait", "", 0);
-  uint8_t p = finger.deleteModel(id);
-  
-  if (p == FINGERPRINT_OK) {
-    lcdPrint("Finger Deleted", "Successfully", 1000);
-  } else {
-    lcdPrint("Something Wrong", "Try Again Later", 2000);
-  }
-  return p;
-}
-
-void download(int eepIndex) {
-  if (EEPROM.read(eepIndex) == 0xff) {
-    Serial.print("--------------------------- ");
-    return;
-  }
-  
-  Serial.print("T->");
-  printDigit(EEPROM.read(eepIndex++));
-  Serial.print(':');
-  printDigit(EEPROM.read(eepIndex++));
-  Serial.print(':');
-  printDigit(EEPROM.read(eepIndex++));
-  Serial.print(" D->");
-  printDigit(EEPROM.read(eepIndex++));
-  Serial.print('/');
-  printDigit(EEPROM.read(eepIndex++));
-  Serial.print('/');
-  Serial.print(EEPROM.read(eepIndex++) << 8 | EEPROM.read(eepIndex++));
-  Serial.print(" ");
-}
-
-void printDigit(byte digit) {
-  if (digit < 10) Serial.print('0');
-  Serial.print(digit);
+void attendance(int id) {
 }
